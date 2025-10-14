f944db6746b9adf7eeb0c96131b1ddfbc42f206a
brandslightdigital <thebrandlightagency@gmail.com>
2025-10-14 16:32:21 +0000

    change:

--- mobility-ssr/vite.config.js
+++ mobility-ssr/vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-
   server: {
     host: true, // Listen on all network interfaces
     allowedHosts: [
